7d6dff80b11ff6061f439448d549d54895f04f69
Yadong Lyu <yaly@microsoft.com>
2025-05-16 13:42:23 -0700

    create main

--- quickstarts/203-fabric-sql-database/main.tf
+++ quickstarts/203-fabric-sql-database/main.tf
@@ -1,9 +1,17 @@
+# The existing capacity name.
 data "fabric_capacity" "capacity" {
   display_name = var.fabric_capacity_name
 }
 
+# Create fabric workspace.
 resource "fabric_workspace" "example_workspace" {
   display_name = var.fabric_workspace_name
   description = "Getting started workspace"
   capacity_id = data.fabric_capacity.capacity.id
+}
+
+# Create fabric SQL Database.
+resource "fabric_sql_database" "example_sql" {
+  workspace_id = fabric_workspace.example_workspace.id
+  display_name = var.fabric_sql_database_name
 }
--- quickstarts/203-fabric-sql-database/sqldatabase.tf
+++
@@ -1,4 +0,0 @@
-resource "fabric_sql_database" "example_sql" {
-  workspace_id = fabric_workspace.example_workspace.id
-  display_name = var.fabric_sql_database_name
-}
